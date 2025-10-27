faee401aaf273ff744f9be30a684bd282dc69e18
jslyzx <821203886@qq.com>
2025-10-27 18:11:22 +0800

    f

--- test-path-alias.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("路径别名测试成功：Button 和 Label 组件可以正常导入");
--- test-path-alias.ts
+++
@@ -1,5 +0,0 @@
-// 简单的测试文件，用于验证路径别名配置
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-
-console.log("路径别名测试成功：Button 和 Label 组件可以正常导入");
